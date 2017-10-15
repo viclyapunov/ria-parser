8d6cce53f1907c37b22699d6828d45f0c4500f5a
Victor Lyapunov <viclyapunov@mail.ru>
2017-10-15 15:29:12 +0000

    first commit, basic working version

---
+++ app/Console/Commands/GetNews.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\DomCrawler\Crawler;
+use App\Article;
+use App\Event;
+use Weidner\Goutte\GoutteFacade as Goutte;
+
+class GetNews extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'news:get';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'download the news from upstream';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $evt = new Event("news:get command started");
+        $evt->save();
+        $crawler = Goutte::request('GET',  'https://ria.ru/lenta/');
+        $crawler->filter('.b-list .b-list__item')->each(function ($node) {
+            $a = new Article();
+            $a->link = $node->filter('a')->attr('href');
+            $a->pic_link = $node->filter('a .b-list__item-img .b-list__item-img-ind img')->attr('src');
+            $a->title = $node->filter('a .b-list__item-title')->text();
+            $a->date = $node->filter('.b-list__item-info .b-list__item-date')->text();
+            $a->time = $node->filter('.b-list__item-info .b-list__item-time')->text();
+            $article = Article::where('link', '=', $a->link)->first();
+            if ($article === null){
+              $a->save();
+            }
+        });
+        $evt = new Event("got news from ria.");
+        $evt->save();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+use Symfony\Component\DomCrawler\Crawler;
+use App\Article;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	return redirect('/articles');
+});
+
+Route::get('/ria', function () {
+	$crawler = Goutte::request('GET',  'https://ria.ru/');
+    $crawler->filter('.b-index__main-list-place li')->each(function ($node) {
+      dump($node->text());
+    });
+    //return view('welcome');
+});
+
+Route::get('/ria-lenta', function () {
+	$crawler = Goutte::request('GET',  'https://ria.ru/lenta/');
+    $crawler->filter('.b-list .b-list__item a .b-list__item-title')->each(function ($node) {
+      dump($node->text());
+    });
+    //return view('welcome');
+});
+
+Route::get('/ria-lenta-links', function () {
+	$crawler = Goutte::request('GET',  'https://ria.ru/lenta/');
+    $crawler->filter('.b-list .b-list__item')->each(function ($node) {
+      $a = new Article();
+      $a->link = $node->filter('a')->attr('href');
+      $a->pic_link = $node->filter('a .b-list__item-img .b-list__item-img-ind img')->attr('src');
+
+      $a->title = $node->filter('a .b-list__item-title')->text();
+      $a->date = $node->filter('.b-list__item-info .b-list__item-date')->text();
+      $a->time = $node->filter('.b-list__item-info .b-list__item-time')->text();
+      $article = Article::where('link', '=', $a->link)->first();
+      if ($article === null)
+      	$a->save();
+    });
+    //return view('welcome');
+});
+
+Route::get('/articles', function () {
+	$articles = Article::all()
+		->sortByDesc("id");
+
+    return view('articles.index', array('articles' => $articles));
+});
